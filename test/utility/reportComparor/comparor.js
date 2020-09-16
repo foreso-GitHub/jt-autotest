31f0b9e2af6734b94176552b5906faa614b0a609
foreso <foreso@sina.com>
2020-09-16 13:40:23 +0800

    更新测试报告baseline。

--- test/utility/reportComparor/comparor.js
+++ test/utility/reportComparor/comparor.js
@@ -16,7 +16,7 @@ async function test(){
     // let file2 = path + 'report2.json'
 
     let path = 'E:\\2. work\\井系\\3. 链景\\井通新链\\自动测试\\codes\\reports'
-    let file1 = path + '\\mochawesome-report-20200916e\\' + 'mochawesome.json'
+    let file1 = path + '\\base-mochawesome-report-20200916f\\' + 'mochawesome.json'
     let file2 = path + '\\mochawesome-report-20200916b\\' + 'mochawesome.json'
 
     let reportsChanges = await reportComparor.compareReportFiles(file1, file2, false)
