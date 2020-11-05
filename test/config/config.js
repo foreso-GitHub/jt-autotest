81fb9e666ade14a688edbc631b99dfcc77b4334e
foreso <foreso@sina.com>
2020-11-05 16:31:34 +0800

    在config中增加各个节点的websocket mode

--- test/config/config.js
+++ test/config/config.js
@@ -20,12 +20,19 @@ let HashMap = require('hashmap')
 let rpc_7545 = new rpc()
 let rpc_9545 = new rpc()
 let rpc_box01 = new rpc()
+
 let rpc_yun_ali = new rpc()
 let rpc_yun_baidu = new rpc()
 let rpc_yun_tengxun = new rpc()
 let rpc_yun_huawei = new rpc()
 let rpc_yun_tianyi = new rpc()
+
 let ws_yun_ali = new ws()
+let ws_yun_baidu = new ws()
+let ws_yun_tengxun = new ws()
+let ws_yun_huawei = new ws()
+let ws_yun_tianyi = new ws()
+
 let lib_main = new swtclib()
 let lib_test = new swtclib()
 let servers = [rpc_7545, rpc_9545, lib_main, lib_test]
@@ -255,6 +262,78 @@ let mode_ws_yun_ali = {  //todo need test ws mode again.
     defaultFee: "10",
     root: {address: "jHb9CJAWyB4jr91VRWn96DkukG4bwdtyTh", secret: "snoPBjXtMeMyMHUVTgbuqAfg1SUTb"},
 }
+
+let mode_ws_yun_baidu = {
+    name: "ws_yun_baidu",
+    server: ws_yun_baidu,
+    initParams: {url:'ws://180.76.125.22:9546/v1/jsonrpc'},
+    chainDataName: "rpc_yun_chain_data",
+    accountsName: "rpc_yun_chain_accounts",
+    service: serviceType.newChain,
+    interface: interfaceType.rpc,
+    testMode: testMode.batchMode,
+    restrictedLevel: restrictedLevel.L3,
+    defaultBlockTime: 5000,
+    retryPauseTime: 1000,
+    retryMaxCount: 16,
+    defaultValue: "1",
+    defaultFee: "10",
+    root: {address: "jHb9CJAWyB4jr91VRWn96DkukG4bwdtyTh", secret: "snoPBjXtMeMyMHUVTgbuqAfg1SUTb"},
+}
+
+let mode_ws_yun_tengxun = {
+    name: "ws_yun_tengxun",
+    server: ws_yun_tengxun,
+    initParams: {url:'ws://45.40.240.50:9546/v1/jsonrpc'},
+    chainDataName: "rpc_yun_chain_data",
+    accountsName: "rpc_yun_chain_accounts",
+    service: serviceType.newChain,
+    interface: interfaceType.rpc,
+    testMode: testMode.batchMode,
+    restrictedLevel: restrictedLevel.L3,
+    defaultBlockTime: 5000,
+    retryPauseTime: 1000,
+    retryMaxCount: 16,
+    defaultValue: "1",
+    defaultFee: "10",
+    root: {address: "jHb9CJAWyB4jr91VRWn96DkukG4bwdtyTh", secret: "snoPBjXtMeMyMHUVTgbuqAfg1SUTb"},
+}
+
+let mode_ws_yun_huawei = {
+    name: "ws_yun_huawei",
+    server: ws_yun_huawei,
+    initParams: {url:'ws://121.37.216.100:9546/v1/jsonrpc'},
+    chainDataName: "rpc_yun_chain_data",
+    accountsName: "rpc_yun_chain_accounts",
+    service: serviceType.newChain,
+    interface: interfaceType.rpc,
+    testMode: testMode.batchMode,
+    restrictedLevel: restrictedLevel.L3,
+    defaultBlockTime: 5000,
+    retryPauseTime: 1000,
+    retryMaxCount: 16,
+    defaultValue: "1",
+    defaultFee: "10",
+    root: {address: "jHb9CJAWyB4jr91VRWn96DkukG4bwdtyTh", secret: "snoPBjXtMeMyMHUVTgbuqAfg1SUTb"},
+}
+
+let mode_ws_yun_tianyi = {
+    name: "ws_yun_tianyi",
+    server: ws_yun_tianyi,
+    initParams: {url:'ws://61.171.12.71:9546/v1/jsonrpc'},
+    chainDataName: "rpc_yun_chain_data",
+    accountsName: "rpc_yun_chain_accounts",
+    service: serviceType.newChain,
+    interface: interfaceType.rpc,
+    testMode: testMode.batchMode,
+    restrictedLevel: restrictedLevel.L3,
+    defaultBlockTime: 5000,
+    retryPauseTime: 1000,
+    retryMaxCount: 16,
+    defaultValue: "1",
+    defaultFee: "10",
+    root: {address: "jHb9CJAWyB4jr91VRWn96DkukG4bwdtyTh", secret: "snoPBjXtMeMyMHUVTgbuqAfg1SUTb"},
+}
 //endregion
 
 //endregion
@@ -293,11 +372,17 @@ let mode_lib_mainnet = {
 //region modes
 let modes = [
     // mode_rpc_yun_ali,
-    // mode_ws_yun_ali,
     // mode_rpc_yun_baidu,
     // mode_rpc_yun_tengxun,
     mode_rpc_yun_huawei,
     // mode_rpc_yun_tianyi,
+
+    // mode_ws_yun_ali,
+    // mode_ws_yun_baidu,
+    // mode_ws_yun_tengxun,
+    // mode_ws_yun_huawei,
+    // mode_ws_yun_tianyi,
+
     // mode_rpc_newChain,
     // mode_rpc_box01,
     // mode_rpc_ipfs,
@@ -311,6 +396,13 @@ let allModes = [
     mode_rpc_yun_huawei,
     mode_rpc_yun_tianyi,
     mode_rpc_yun_ali,
+
+    mode_ws_yun_ali,
+    mode_ws_yun_baidu,
+    mode_ws_yun_tengxun,
+    mode_ws_yun_huawei,
+    mode_ws_yun_tianyi,
+
     // mode_rpc_newChain,
     // mode_rpc_box01,
     // mode_rpc_box02,
