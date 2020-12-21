17b69fd3f006bb030bedabaa30d04c2b0024d369
foreso <foreso@sina.com>
2020-12-21 15:47:12 +0800

    更新linux压测部署脚本，更新测试数据。

---
+++ doc/linux部署/basicInstall.sh
@@ -0,0 +1,6 @@
+sudo su
+apt update
+apt install npm
+apt install git
+npm install -g gulp
+npm install -g mocha
--- doc/linux部署/deploy.sh
+++ doc/linux部署/deploy.sh
@@ -1,8 +1,9 @@
+sudo su
+cd /root
 mkdir codes
 cd codes
 rm jt-autotest -fr
 git clone https://github.com/foreso-GitHub/jt-autotest.git
 cd jt-autotest
-npm install -g mocha
-npm install -g gulp
 npm install
+
