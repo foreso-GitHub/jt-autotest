60f1dad33fccd668022b8ef34a9453cfb95af880
foreso <foreso@sina.com>
2021-01-21 16:57:12 +0800

    增加测试结果数据

--- test/utility/tools.js
+++ test/utility/tools.js
@@ -128,9 +128,9 @@ function printBar(tps){
 async function compare(){
 
     let path = 'E:\\2. work\\井系\\3. 链景\\井通新链\\自动测试\\codes\\reports'
-    let file1 = path + '\\baselines\\base-mochawesome-report-20201231a-no_exp-ws\\' + 'mochawesome.json'
+    let file1 = path + '\\baselines\\base-mochawesome-report-20210121a-no_exp-ws\\' + 'mochawesome.json'
     // let file1 = path + '\\normal\\mochawesome-report-20210115a-no_exp-ws\\' + 'mochawesome.json'
-    let file2 = path + '\\normal\\mochawesome-report-20210118b-no_exp-ws\\' + 'mochawesome.json'
+    let file2 = path + '\\normal\\mochawesome-report-20210121a-no_exp-ws\\' + 'mochawesome.json'
 
     let reportsChanges = await reportComparor.compareReportFiles(file1, file2, false)
 
