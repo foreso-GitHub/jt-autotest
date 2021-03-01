e13b4b6fafbb593d926906e106a88f2b19406a64
foreso <foreso@sina.com>
2021-03-01 17:14:28 +0800

    根据新版本调整部分info

--- test/testCases/tcsSequenceTest.js
+++ test/testCases/tcsSequenceTest.js
@@ -131,7 +131,7 @@ module.exports = tcsSequenceTest = {
             testScript.actions[0].bindData.plusValueTimes = 0 // value should not change
             testScript.actions[0].bindData.timeoutAfterSend = 0  //need not timeout
             let expectedResult = framework.createExpectedResult(false,
-                framework.getError(-284, 'sequence is not integer'))
+                framework.getError(-284, 'sequence is not a valid integer'))
             framework.changeExpectedResultWhenSignFail(testScript, expectedResult)
 
             framework.addTestScript(testScripts, testScript)
@@ -163,7 +163,7 @@ module.exports = tcsSequenceTest = {
             testScript.actions[0].bindData.plusValueTimes = 0 // value should not change
             testScript.actions[0].bindData.timeoutAfterSend = 0  //need not timeout
             let expectedResult = framework.createExpectedResult(false,
-                framework.getError(-284, 'sequence is not integer'))
+                framework.getError(-284, 'sequence is not a valid integer'))
             framework.changeExpectedResultWhenSignFail(testScript, expectedResult)
 
             framework.addTestScript(testScripts, testScript)
